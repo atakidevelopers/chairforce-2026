46b912eaedd4decfaa492caf14fdbb7de9257703
AtakiDev <developers@ataki.dev>
2026-08-01 20:23:25 +0500

    Scope YITH quote icon styles to the button element only.
    Move cf-icon-button and inline-flex onto .add-request-quote-button so browse-message links are not styled as icon buttons.

--- wp-content/themes/chairforce/build/public.asset.php
+++ wp-content/themes/chairforce/build/public.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => 'd488fd0d70d77c8c90ab');
+<?php return array('dependencies' => array('jquery'), 'version' => 'cd866eec86453e431a51');
